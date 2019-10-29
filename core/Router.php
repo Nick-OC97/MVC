6f3f6a6ac0fec06236c8603e798e927b4008191d
Nick-OC97 <no-conne@student.wethinkcode.co.za>
2019-10-29 14:54:08 +0200

    finished basics structures

--- core/Router.php
+++ core/Router.php
@@ -5,7 +5,27 @@
 		public static function route($url)
 		{
 			//controller
-			$controller = (isset($url[0]) && url[0] != '') ? ucwords($url[0]): DEFAULT_CONTROLLER;
-			dnd($controller);
+			$controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]): DEFAULT_CONTROLLER;
+			$controller_name = $controller;
+			array_shift($url);
+
+			//action
+			$action = (isset($url[0]) && $url[0] != '') ? $url[0] . 'Action': 'indexAction';
+			$action_name = $controller;
+			array_shift($url);
+
+			//params
+			$queryParams = $url;
+
+			$dispach = new $controller($controller_name, $action);
+
+			if (method_exists($controller, $action))
+			{
+				call_user_func_array([$dispatch, $action], $queryParams) ; 
+			}
+			else
+			{
+				die('That method doesn not exist in the controller \"' . $controller_name . '\"');
+			}
 		}
 	}

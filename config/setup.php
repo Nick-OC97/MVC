5006db47ca29a9e01f4987622c1f2dea418659c6
Nick-OC97 <no-conne@student.wethinkcode.co.za>
2019-12-10 10:36:06 +0200

    added table data

--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,5 @@
-<?php
 
-	'CREATE TABLE `contacts` (
+	CREATE TABLE `contacts` (
 	`id` int(11) NOT NULL,
 	`fname` varchar(150) NOT NULL,
 	`lname` varchar(155) NOT NULL,
@@ -15,6 +14,24 @@
 	`zip_code` varchar(50) NOT NULL,
 	`country` varchar(155) NOT NULL,
 	`user_id` int(11) NOT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-?>
+
+	CREATE TABLE `users` (
+	`id` int(11) NOT NULL,
+	`username` varchar(150) NOT NULL,
+	`email` varchar(150) NOT NULL,
+	`password` varchar(150) NOT NULL,
+	`fname` varchar(150) NOT NULL,
+	`lname` varchar(150) NOT NULL,
+	`acl` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+	`deleted` tinyint(4) DEFAULT '0'
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+	CREATE TABLE `user_sessions` (
+	`id` int(11) NOT NULL,
+	`user_id` int(11) NOT NULL,
+	`session` varchar(255) NOT NULL,
+	`user_agent` varchar(255) NOT NULL
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
